960c0c04d2d12c41098557c5cda95f2438b3551e
YoungKwang-Kim <kyk01910691@gmail.com>
2024-03-21 20:05:04 +0900

    선 그리기 기능 1차 구현 완료했습니다.

--- Assets/Scripts/Damage Analyze Scene/DrawLine.cs
+++ Assets/Scripts/Damage Analyze Scene/DrawLine.cs
@@ -1,59 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DrawLine : MonoBehaviour
 {
-    // 선을 그릴 LineRenderer 컴포넌트
-    private LineRenderer lineRenderer;
+    // Line Renderer 프리팹
+    public GameObject linePrefab;
 
-    // 선의 시작점과 끝점을 저장할 변수
-    private Vector3 startPoint;
-    private Vector3 endPoint;
+    LineRenderer lr;
+    // 클릭했을 때의 마우스의 포지션 값을 저장한다.
+    List<Vector3> points = new List<Vector3>();
 
-    void Start()
+
+    private void Update()
     {
-        // LineRenderer 컴포넌트를 추가합니다.
-        lineRenderer = gameObject.AddComponent<LineRenderer>();
-
-        // 선의 두께 설정
-        lineRenderer.startWidth = 0.1f;
-        lineRenderer.endWidth = 0.1f;
-
-        // 선 색상 설정
-        lineRenderer.startColor = Color.red;
-        lineRenderer.endColor = Color.blue;
-
-        // 점의 갯수 설정 (여기서는 시작점과 끝점을 설정할 것이므로 2로 설정)
-        lineRenderer.positionCount = 2;
-
-        // 초기화 상태에서는 선을 보이지 않도록 설정
-        lineRenderer.enabled = false;
-    }
-
-    void Update()
-    {
-        // 마우스 왼쪽 버튼이 클릭되었을 때
-        if (Input.GetMouseButtonDown(0))
+        // 첫번째 좌클릭
+        if (Input.GetMouseButtonDown(0) && points.Count == 0)
         {
-            // 선을 보이도록 설정
-            lineRenderer.enabled = true;
-
-            // 시작점을 마우스 클릭한 위치로 설정
-            startPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            startPoint.z = 0; // z 값은 Canvas 평면에 고정되어야 합니다.
-
-            // 선의 시작점 설정
-            lineRenderer.SetPosition(0, startPoint);
+            GameObject go = Instantiate(linePrefab, gameObject.transform);
+            lr = go.GetComponent<LineRenderer>();
+            points.Add(Input.mousePosition);
+            lr.positionCount = 1;
+            lr.SetPosition(0, new Vector3(points[0].x, points[0].y, -36));
         }
-
-        // 마우스 왼쪽 버튼이 떼어졌을 때
-        if (Input.GetMouseButtonUp(0))
+        // 두번째 좌클릭
+        else if (Input.GetMouseButtonDown(0) && points.Count == 1)
         {
-            // 끝점을 마우스 클릭한 위치로 설정
-            endPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            endPoint.z = 0; // z 값은 Canvas 평면에 고정되어야 합니다.
-
-            // 선의 끝점 설정
-            lineRenderer.SetPosition(1, endPoint);
+            points.Add(Input.mousePosition);
+            lr.positionCount = 2;
+            lr.SetPosition(1, new Vector3(points[1].x, points[1].y, -36));
         }
     }
 }
