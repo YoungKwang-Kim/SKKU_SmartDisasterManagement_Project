08711c06ec51bb038d82e702f42c4ece5669af0d
YoungKwang-Kim <kyk01910691@gmail.com>
2024-03-21 23:43:23 +0900

    Skybox 적용했습니다.

--- Assets/Scripts/Drone Inspection/DronePathRenderer.cs
+++ Assets/Scripts/Drone Inspection/DronePathRenderer.cs
@@ -5,13 +5,13 @@ public class DronePathRenderer : MonoBehaviour
     public LineRenderer lineRenderer;
     public Transform[] waypoints; // 드론이 이동할 waypoint들을 저장하는 배열
 
-    void Update()
-    {
-        // 드론의 현재 위치를 저장하여 라인 렌더러에 전달
-        UpdateLineRenderer();
-    }
+    // 라인 렌더러가 사라지는 시간 (초)
+    public float fadeDuration = 1.0f;
 
-    void UpdateLineRenderer()
+    // 시작 시간
+    private float startTime;
+
+    private void Start()
     {
         lineRenderer.positionCount = waypoints.Length;
         for (int i = 0; i < waypoints.Length; i++)
@@ -19,13 +19,7 @@ public class DronePathRenderer : MonoBehaviour
             lineRenderer.SetPosition(i, waypoints[i].position);
         }
 
-        // 투명도를 조절하여 사라지는 효과 추가
-        float alpha = 1f;
-        for (int i = 0; i < lineRenderer.positionCount; i++)
-        {
-            lineRenderer.startColor = new Color(lineRenderer.startColor.r, lineRenderer.startColor.g, lineRenderer.startColor.b, alpha);
-            lineRenderer.endColor = new Color(lineRenderer.endColor.r, lineRenderer.endColor.g, lineRenderer.endColor.b, alpha);
-            alpha -= 0.1f; // 투명도를 점차적으로 감소시켜 사라지는 효과 구현
-        }
+        // 시작 시간 초기화
+        startTime = Time.time;
     }
 }
